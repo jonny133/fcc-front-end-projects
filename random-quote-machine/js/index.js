d24ac7c059989c7fd5315e9fe4f2c678851cb903
Jonny <1474476+jonny133@users.noreply.github.com>
2018-11-16 16:41:20 +0000

    Get quotes from external API

--- random-quote-machine/js/index.js
+++ random-quote-machine/js/index.js
@@ -1,4 +1,3 @@
-var _slicedToArray = function () {function sliceIterator(arr, i) {var _arr = [];var _n = true;var _d = false;var _e = undefined;try {for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {_arr.push(_s.value);if (i && _arr.length === i) break;}} catch (err) {_d = true;_e = err;} finally {try {if (!_n && _i["return"]) _i["return"]();} finally {if (_d) throw _e;}}return _arr;}return function (arr, i) {if (Array.isArray(arr)) {return arr;} else if (Symbol.iterator in Object(arr)) {return sliceIterator(arr, i);} else {throw new TypeError("Invalid attempt to destructure non-iterable instance");}};}(); 
 
 var quotes = [
 ["Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you.", "Princess Diana"],
@@ -7,14 +6,25 @@ var quotes = [
 ["One of the penalties of refusing to participate in politics is that you end up being governed by your inferiors.", "Plato"],
 ['I\'m not the smartest fellow in the world, but I can sure pick smart colleagues.', 'Franklin D. Roosevelt']];
 
+// var getNewQuote = () => {
+//   let [qt, athr] = quotes[Math.floor(Math.random()*quotes.length)];
+//   $('#text').text(qt);
+//   $('#author #athr').text(athr);
 
-var getNewQuote = function getNewQuote() {var _quotes$Math$floor = _slicedToArray(
-  quotes[Math.floor(Math.random() * quotes.length)], 2),qt = _quotes$Math$floor[0],athr = _quotes$Math$floor[1];
-  $('#text').text(qt);
-  $('#author #athr').text(athr);
+//   $('a#tweet-quote').attr('href', "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+qt+' - '+athr);
+// };
 
-  $('a#tweet-quote').attr('href', "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + qt + ' - ' + athr);
+var getNewQuote = function getNewQuote() {
+  // $.getJSON('https://api.chucknorris.io/jokes/random?category=dev').done(
+  //   (data) = >{
+  //   $('#text').text(data.value);
+  //   $('#athr').text(data.url);
+  //   console.log(data);
+  // });
+  $.get('https://talaikis.com/api/quotes/random/', function (data) {console.log(data);
+    $('#text').html(data.quote);$('#athr').text(data.author);});
 };
+
 
 $(document).ready(function () {
   getNewQuote();
